3c066d7a8fe51c07a6a5dd74b29446fcf9c3ea09
Liam McAteer <liam@liammcateer.nz>
2020-05-17 16:13:01 +1200

    added shared header and navigation for frontend

--- routers/frontendRouter.js
+++ routers/frontendRouter.js
@@ -9,4 +9,12 @@ router.route('/').get(function(req, res) {
     res.sendFile(path.join(__dirname, '../', '/frontend/index.html'));
 });
 
+router.route('/head').get(function(req, res) {
+    res.sendFile(path.join(__dirname, '../', '/frontend/head.html'));
+});
+
+router.route('/nav').get(function(req, res) {
+    res.sendFile(path.join(__dirname, '../', '/frontend/nav.html'));
+});
+
 module.exports = router;
