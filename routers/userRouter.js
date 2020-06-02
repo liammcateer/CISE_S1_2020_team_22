ac04e1a24a9ad388b9ef1c3152f060af3b192be5
John Du <gzdhy8895@gmail.com>
2020-06-02 15:43:24 +1200

    update

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -5,15 +5,15 @@ const userController = require('../controllers/userController');
 const router = express.Router();
 
 // eslint-disable-next-line prettier/prettier
-router
-  .route('/')
-  .post(userController.submitArticle)
-  .patch(userController.updateArticle)
-  .get(userController.getAllUsers);
+// router
+//   .route('/')
+//   .post(userController.submitArticle)
+//   .patch(userController.updateArticle)
+//   .get(userController.getAllUsers);
 
-router
-  .route('/authorize')
-  .post(userController.signup)
-  .get(userController.login);
+// router
+//   .route('/authorize')
+//   .post(userController.signup)
+//   .get(userController.login);
 
 module.exports = router;
